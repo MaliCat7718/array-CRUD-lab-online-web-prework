5aabcee1e6a1045ff796bf1b61805f51a2fbcc56
Malary Robinson <robinmal16@gmail.com>
2019-04-26 01:55:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,9 @@ end
 
 def remove_element_from_end_of_array(array)
 array = ["I","am","really","learning","arrays!"]
-array.pop"wow"
+wow = array.pop
+puts array.inspect
+puts wow
 end
 
 def remove_element_from_start_of_array(array)
